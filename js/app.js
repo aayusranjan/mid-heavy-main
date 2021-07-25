f5b1e5188e87f7d7cb859712efc9bf0d0f5ba3da
Chris Steffes <c.steffes@live.ca>
2021-07-24 21:05:59 -0500

    time multiplier + cleanup

--- js/app.js
+++ js/app.js
@@ -1,11 +1,64 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-
+/*               --- Particle JS ---               */
 particlesJS.load("particles-js", "particles.json", function () {
-  console.log("particles.js loaded - callback");
-});
+  console.log("particles.js loaded - callback")
+})
 
-/* Otherwise just put the config content (json): */
+/*       --- Mid Heavy Timeline Animation ---       */
+var basicTimeline = anime.timeline()
+basicTimeline.reverse()
+var TIME_MULTIPLIER = 1.45
+
+function H() {
+  // Make the line visible
+  document.getElementById("hPath").style.display = "block"
+  return {
+    targets: "#hPath",
+    translateX: 100,
+    direction: "reverse",
+    easing: "easeInOutExpo",
+    duration: TIME_MULTIPLIER * 150,
+    delay: TIME_MULTIPLIER * 100
+  }
+}
+
+function Back(backTarget) {
+  return {
+    targets: backTarget,
+    translateY: 212,
+    translateX: -35,
+    direction: "reverse",
+    easing: "linear",
+    duration: TIME_MULTIPLIER * 110
+  }
+}
+
+function Front(frontTarget) {
+  return {
+    targets: frontTarget,
+    translateY: -212,
+    translateX: -35,
+    direction: "reverse",
+    easing: "linear",
+    duration: TIME_MULTIPLIER * 110
+  }
+}
+
+// This runs from the bottom up
+basicTimeline
+  // **End Here**
+  .add(Front(`#frontPath5`))
+  .add(Back(`#backPath5`))
+  .add(H())
+
+  .add(Front(`#frontPath4`))
+  .add(Back(`#backPath4`))
+
+  .add(Front(`#frontPath3`))
+  .add(Back(`#backPath3`))
+
+  .add(Front(`#frontPath2`))
+  .add(Back(`#backPath2`))
+
+  .add(Front(`#frontPath1`))
+  .add(Back(`#backPath1`))
+// **Start Here**
